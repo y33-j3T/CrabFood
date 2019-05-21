96b2a3d512ff9e99fb11a8c2690f1a7460606710
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2019-05-21 21:54:18 +0800

    minor fixes

--- src/crabfood/CrabFoodOperator.java
+++ src/crabfood/CrabFoodOperator.java
@@ -20,8 +20,6 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 
 class CrabFoodOperator {
 
